f4cd9a3dfb09788ff962bb507a3a8145e0bf0d96
changzhen976 <changzhen976@gmail.com>
2024-07-10 16:55:53 +0800

    modify addPort script     - add try catch code skip crash from add same port-name

--- Models/testModel/AutoSarArchScript.m
+++ Models/testModel/AutoSarArchScript.m
@@ -10,12 +10,36 @@
 archModel = autosar.arch.loadModel(gcs);
 
 %% find composion & component
+ComponentsCell = {};
+CompositionsCell = {};
 
-comp1 = archModel.Components;
-comp2 = archModel.Compositions;
+if isempty(archModel.Components)
 
-addPort(comp1,'Receiver','ThrCmd_Int');
-addPort(comp2,'Sender','ThrCmd_Int');
+else
+    for i = 1:length(archModel.Components)
+        ComponentsCell = [ComponentsCell, archModel.Components(i)];
+    end
+end
+
+if isempty(archModel.Compositions)
+
+else
+    for i = 1:length(archModel.Compositions)
+        CompositionsCell = [CompositionsCell, archModel.Compositions(i)];
+    end
+end
+
+
+comp1 = ComponentsCell(1);
+comp2 = CompositionsCell(1);
+
+%% add port
+
+addCompPort(comp1,'Receiver','ThrCmd_Int');
+addCompPort(comp1,'Receiver','ThrCmd_Int2');
+addCompPort(comp2,'Sender','ThrCmd_Int');
+addCompPort(comp2,'Sender','ThrCmd_Int2');
+
 
 
 %% connect
@@ -23,7 +47,17 @@ addPort(comp2,'Sender','ThrCmd_Int');
 % connect(archModel,<comp with outport>,<comp with inport>);
 connectors = connect(archModel,comp2,comp1);
 
+%% functions
 
+function addCompPort(comp,SendReceive, PortName)
+
+try
+    addPort(comp, SendReceive, PortName);
+catch errMsg
+    warning(errMsg.message);
+end
+
+end
 
 
 
