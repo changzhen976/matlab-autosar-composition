8f1424df6d5381f1a1af38b7162b76aec56c0f56
changzhen976 <changzhen976@gmail.com>
2024-07-10 17:41:31 +0800

    add addPort script    - add components/composition port use 'addPort'

---
+++ App/SarConnect/SarAddPort.m
@@ -0,0 +1,70 @@
+
+
+function SarAddPort(CompCell,SendReceive, PortName)
+
+for i = 1:length(CompCell)
+
+end
+    addCompPort(CompCell(i),SendReceive, PortName);
+
+end
+%% load Arch Model
+
+% modelName = 'testModel4Script';
+archModel = autosar.arch.loadModel(gcs);
+
+%% find composion & component
+ComponentsCell = {};
+CompositionsCell = {};
+
+if isempty(archModel.Components)
+
+else
+    for i = 1:length(archModel.Components)
+        ComponentsCell = [ComponentsCell, archModel.Components(i)];
+    end
+end
+
+if isempty(archModel.Compositions)
+
+else
+    for i = 1:length(archModel.Compositions)
+        CompositionsCell = [CompositionsCell, archModel.Compositions(i)];
+    end
+end
+
+
+comp1 = ComponentsCell(1);
+comp2 = CompositionsCell(1);
+
+%% add port
+
+addCompPort(comp1,'Receiver','ThrCmd_Int');
+addCompPort(comp1,'Receiver','ThrCmd_Int2');
+addCompPort(comp2,'Sender','ThrCmd_Int');
+addCompPort(comp2,'Sender','ThrCmd_Int2');
+
+
+
+%% connect
+
+% connect(archModel,<comp with outport>,<comp with inport>);
+connectors = connect(archModel,comp2,comp1);
+
+%% functions
+
+function addCompPort(comp,SendReceive, PortName)
+
+try
+    addPort(comp, SendReceive, PortName);
+catch errMsg
+    warning(errMsg.message);
+end
+
+end
+
+
+
+
+
+
